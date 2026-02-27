f4a2dfe59869a048d2a170d54bd6b0a1cd3c4b78
ammon <ammoncliften@gmail.com>
2026-02-27 00:26:30 -0700

    Implemented the basics for Register, Login, and Logout endpoints

--- server/src/main/java/server/UserHandler.java
+++ server/src/main/java/server/UserHandler.java
@@ -1,6 +1,7 @@
 package server;
 
 import com.google.gson.Gson;
+import dataaccess.DataAccessException;
 import io.javalin.http.Context;
 import records.*;
 import service.UserService;
@@ -9,21 +10,21 @@ public class UserHandler {
     UserService userService = new UserService();
     Gson serializer = new Gson();
 
-    public void register(Context ctx){
+    public void register(Context ctx) throws DataAccessException {
         RegisterRequest request = serializer.fromJson(ctx.body(), RegisterRequest.class);
         RegisterResult result = userService.register(request);
 
         ctx.result(serializer.toJson(result));
     }
 
-    public void login(Context ctx){
+    public void login(Context ctx) throws DataAccessException {
         LoginRequest request = serializer.fromJson(ctx.body(), LoginRequest.class);
         LoginResult result = userService.login(request);
 
         ctx.result(serializer.toJson(result));
     }
 
-    public void logout(Context ctx){
+    public void logout(Context ctx) throws DataAccessException {
         LogoutRequest request = new LogoutRequest(ctx.header("authorization"));
         userService.logout(request);
     }
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,15 +1,59 @@
 package service;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import io.javalin.http.UnauthorizedResponse;
+import model.AuthData;
+import model.UserData;
 import records.*;
+import java.util.UUID;
 
 public class UserService {
-    public RegisterResult register(RegisterRequest registerRequest) {
-        return null;
+    MemoryUserDAO userDAO = new MemoryUserDAO();
+    MemoryAuthDAO authDAO = new MemoryAuthDAO();
+
+    public UserService(){
+
     }
-    public LoginResult login(LoginRequest loginRequest) {
-        return new LoginResult("userrrr", "realtoken");
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
     }
-    public void logout(LogoutRequest logoutRequest) {
-        
+
+    public RegisterResult register(RegisterRequest registerRequest) throws DataAccessException {
+        if (userDAO.getUser(registerRequest.username()) == null){
+            UserData user = new UserData(registerRequest.username(), registerRequest.password(), registerRequest.email());
+            userDAO.createUser(user);
+            String authToken = generateToken();
+            AuthData auth = new AuthData(authToken, user.username());
+            authDAO.createAuth(auth);
+
+            return new RegisterResult(user.username(), authToken);
+        }
+        throw new DataAccessException("Error: already taken");
+    }
+
+    public LoginResult login(LoginRequest loginRequest) throws DataAccessException {
+        UserData user = userDAO.getUser(loginRequest.username());
+        if (user == null){
+            throw new DataAccessException("Error: unauthorized");
+        }
+        if (loginRequest.password().equals(user.password())){
+            String authToken = generateToken();
+            AuthData auth = new AuthData(authToken, user.username());
+            authDAO.createAuth(auth);
+
+            return new LoginResult(user.username(), auth.authToken());
+        }
+        throw new DataAccessException("Error: unauthorized");
+    }
+
+    public void logout(LogoutRequest logoutRequest) throws DataAccessException {
+        if (authDAO.getAuth(logoutRequest.authorization()) != null){
+            authDAO.deleteAuth(logoutRequest.authorization());
+        } else {
+            throw new DataAccessException("Error: unauthorized");
+        }
     }
 }
 
