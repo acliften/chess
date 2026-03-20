73e103f7675c0f9f39ca25b77b4055d76fe6b649
ammon <ammoncliften@gmail.com>
2026-03-20 15:55:52 -0600

    made the basics for the repl

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,38 @@
+package client;
+
+public class ServerFacade {
+
+    public String register(String[] params){
+        return "";
+
+    }
+
+    public String login(String[] params){
+        return "";
+
+    }
+
+    public String createGame(String[] params){
+        return "";
+
+    }
+
+    public String listGames(){
+        return "";
+
+    }
+
+    public String joinGame(String[] params){
+        return "";
+    }
+
+    public String observeGame(String[] params){
+        return "";
+    }
+
+    public String logout(){
+        return "";
+    }
+
+
+}
--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -1,8 +1,14 @@
 package ui;
 
+import client.ServerFacade;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Client {
+
+    boolean loggedIn = false;
+    ServerFacade sf = new ServerFacade();
 
     public void run() {
         System.out.println("hi");
@@ -13,6 +19,56 @@ public class Client {
             System.out.print("\n" + ">>> ");
             String line = scanner.nextLine();
 
+            try {
+                result = eval(line);
+                System.out.println(result);
+            } catch (Throwable e){
+                System.out.println(e.toString());
+            }
+
         }
     }
+
+    public String eval(String input){
+        try {
+            String[] tokens = input.toLowerCase().split(" ");
+            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> sf.register(params);
+                case "login" -> sf.login(params);
+                case "create" -> sf.createGame(params);
+                case "list" -> sf.listGames();
+                case "join" -> sf.joinGame(params);
+                case "observe" -> sf.observeGame(params);
+                case "logout" -> sf.logout();
+                case "help" -> help();
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (Exception e){
+            return e.getMessage();
+        }
+    }
+
+    public String help() {
+        if (!loggedIn) {
+            return """
+                    - register <USERNAME> <PASSWORD> <EMAIL> - to create an account
+                    - login <USERNAME> <PASSWORD>
+                    - quit - exit program
+                    - help - receive this instruction again
+                    """;
+        }
+        return """
+                - create <GAME_NAME> - creates a game
+                - list - lists available games
+                - join <ID> [WHITE|BLACK] - joins a game
+                - observe <ID> - spectate a game
+                - logout - log out of your account
+                - quit - exit program
+                - help - shows this list again
+                """;
+    }
+
 }
